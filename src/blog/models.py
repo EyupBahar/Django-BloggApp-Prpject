f6b7925ca3128e3bbc0512c1a3a6da0a1629b7b0
7204-OMER <Eyupbhr24@gmail.com>
2021-08-19 20:35:55 +0300

    add models.py

--- src/blog/models.py
+++ src/blog/models.py
@@ -6,52 +6,66 @@ def user_directory_path(instance, filename):
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
-
     
     class Meta:
         verbose_name_plural = "Categories"
-
+        
     def __str__(self):
-        return self.name    
+        return self.name
+
 class Post(models.Model):
     OPTIONS = (
-        ('draft', 'Draft'),
-        ('published', 'Published'),
+        ('d', 'Draft'),
+        ('p', 'Published')
     )
-    title = models.CharField(max_length=100)
+  
+    title = models.CharField(max_length=100)    
     content = models.TextField()
-    image = models.ImageField(upload_to=user_directory_path, default='Django.png')
-    category = models.ForeignKey(Category, on_delete=models.PROTECT)
+    image = models.ImageField(upload_to = user_directory_path, default='Django.jpg')
+    category = models.ForeignKey(Category, on_delete=models.PROTECT, blank=True, null=True)
     publish_date = models.DateTimeField(auto_now_add=True)
-    last_updated = models.DateTimeField(auto_now=True)
+    # last_updated = models.DateTimeField(auto_now=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    status = models.CharField(max_length=10, choices=OPTIONS, default='draft')
-    slug = models.SlugField(blank=True, unique=True)
-
+    status = models.CharField(max_length=10, choices=OPTIONS, default='d')
+    slug = models.SlugField(blank=True)
 
     def __str__(self):
         return self.title
+    
+    
+    
+    def comment_count(self):
+        return self.comment_set.all().count()
+    
+    def view_count(self):
+        return self.postview_set.all().count()
+    
+    def like_count(self):
+        return self.like_set.all().count()
+    
+    def comments(self):
+        return self.comment_set.all()
 
 class Comment(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comment")
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     time_stamp = models.DateTimeField(auto_now_add=True)
     content = models.TextField()
-
+    
     def __str__(self):
         return self.user.username
-
+    
 class Like(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.user.username
-
+    
 class PostView(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     time_stamp = models.DateTimeField(auto_now_add=True)
-
+    
     def __str__(self):
         return self.user.username
