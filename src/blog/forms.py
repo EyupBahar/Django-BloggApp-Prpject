a4bd0bfd102db716ee79e1326ea55de8430963e0
7204-OMER <Eyupbhr24@gmail.com>
2021-08-19 17:37:44 +0300

    add forms.py updates

--- src/blog/forms.py
+++ src/blog/forms.py
@@ -1,8 +1,22 @@
 from django import forms
-from .models import Post, Comment, Comment
-
+from .models import Post, Commen, Category
 
 class PostForm(models.Model):
+    status = forms.ChoiceField(choices=Post.OPTIONS)
+    category = forms.ModelChoiceField(
+        queryset=Category.objects.all(), empty_label="Select")
+         
     class Meta:
         model = Post
-        fields = (' ')
+        fields = (
+            'title',
+            'image',
+            'content',
+            'category',
+            'status',
+            )
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ('content',)
