db206ec0efbc27ae7c60a075ca94350a35fc0adf
7204-OMER <Eyupbhr24@gmail.com>
2021-08-19 17:38:10 +0300

    add views

--- src/blog/views.py
+++ src/blog/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from .models import Post
+from .forms import PostForm
 
-# Create your views here.
+def post_list(request):
+    qs = Post.objects.all()
+    context = {
+        "object_list":qs
+    }
+    return render(request, "blog/post_list.html", context)
+
+def post_create(request):
+    form = PostForm()
+    if request == "POST":
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            return redirect("blog:list")
+
+    context = {
+        'form' : form
+    }            
+    return render(request, "blog/post_create.html", context)
