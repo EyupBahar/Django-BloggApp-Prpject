6a83973a3b123a523b50204d7786a88042b798ef
7204-OMER <Eyupbhr24@gmail.com>
2021-08-19 22:57:13 +0300

    add signals.py

--- src/blog/signals.py
+++ src/blog/signals.py
@@ -2,12 +2,13 @@ from django.db.models.signals import pre_save
 from django.dispatch import receiver
 from django.template.defaultfilters import slugify
 from .models import Post
+from .utils import get_random_code
 
 @receiver(pre_save, sender=Post)
 def pre_save_create_slug(sender, instance, **kwargs):
     if not instance.slug:
         instance.slug = slugify(
-            instance.author.username + " " + instance.title)
+            instance.title + " " + get_random_code())
 
 # a = "Omer Faruk"
 
