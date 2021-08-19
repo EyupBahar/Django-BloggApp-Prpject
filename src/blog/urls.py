6f492019ddf1fbf1614a70afe81a5c20b45db5e1
7204-OMER <Eyupbhr24@gmail.com>
2021-08-19 20:36:04 +0300

    add urls.py

--- src/blog/urls.py
+++ src/blog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import post_list
+from .views import post_list, post_create
 
 app_name = "blog"
 urlpatterns = [
-    path("", post_list, name="list" )
+    path("", post_list, name="list" ),
+    path("create/", post_create, name="create" )
 ]
