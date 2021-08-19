f568a40938f19c49c5adeff4ef98fa374d7d9f04
7204-OMER <Eyupbhr24@gmail.com>
2021-08-19 11:27:13 +0300

    add settings.py

--- src/myblog/settings.py
+++ src/myblog/settings.py
@@ -120,6 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = BASE_DIR / "media_root"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
