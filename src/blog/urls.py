cd5a6b92778f49abf1c390fbe003934002c5231a
7204-OMER <Eyupbhr24@gmail.com>
2021-08-20 12:03:46 +0300

    add urls.py updates

--- src/blog/urls.py
+++ src/blog/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import post_list, post_create
+from .views import post_list, post_create, post_detail, post_update, post_delete
 
 app_name = "blog"
 urlpatterns = [
     path("", post_list, name="list" ),
-    path("create/", post_create, name="create" )
+    path("create/", post_create, name="create" ),
+    path("<str:slug>/", post_create, name="detail" ),
+    path("<str:slug>/update", post_update, name="update" ),
+    path("<str:slug>/delete", post_delete, name="delete" )
 ]
